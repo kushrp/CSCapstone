a6973c1829460ef749c24dae387b881b41c30d4d
Kartik Yadav <yadav11@purdue.edu>
2016-12-06 19:03:56 -0500

    Date picker for projects

--- ProjectsApp/forms.py
+++ ProjectsApp/forms.py
@@ -4,8 +4,14 @@ from django import forms
 class ProjectForm(forms.Form):
   name = forms.CharField(label='Name', max_length=50, widget=forms.TextInput)
   description = forms.CharField(label='Description', max_length=300, widget=forms.TextInput)
-  created_at = forms.DateTimeField(label="Date created at", widget=forms.DateTimeInput)
-  updated_at = forms.DateTimeField(label="Date updated at", widget=forms.DateTimeInput)
+  created_at = forms.DateTimeField(label="Date created at", widget=forms.TextInput(attrs=
+                                {
+                                    'class':'datepicker'
+                                }))
+  updated_at = forms.DateTimeField(label="Date updated at", widget=forms.TextInput(attrs=
+                                {
+                                    'class':'datepicker'
+                                }))
   languages = forms.CharField(label="Programming Languages", max_length=1000,widget=forms.TextInput)
   speciality = forms.CharField(label="Speciality", max_length=100,widget=forms.TextInput)
   years = forms.IntegerField(label="Experience of Programming experience",widget=forms.NumberInput)
